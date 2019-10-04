dff689c7fe99e6a0c5f26c6c388f5260c1426c78
MoSadie <MoSadie@users.noreply.github.com>
2019-10-03 20:50:53 -0700

    Inital Functionality
    What this does:
    - Adds scripts to automatically load anaconda and compile / program code.
    - Adds VSCode tasks for building & programming in both release and debug profiles. (This means you can build/program from VSCode)
    - Sets the Debug launch config to run the debug program task before running automatically.
    - Sets the release program task to be the default build task. (Default Keybinding: Ctrl + Shift + B)
    - Created a 'scripts' folder to hold executable scripts. (Also moved the linter.sh file in there)
    
    How to use:
    - If anaconda is not installed in the default location, set the enviroment variable Anaconda to the anaconda3 folder.
    - After that, tasks can be run from the command pallate. Debugging automatically runs the program debug task before starting debugging.
    
    Limitations:
    - Currently Windows-only. (Added error message if run on a different platform)

---
+++ scripts/windows/debugProgram.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Changing directory into project...
+cd mcb-2019-2020-project
+if exist %AppData%\..\Local\Continuum\anaconda3\Scripts\activate.bat (
+    echo Activating Anaconda...
+    call %AppData%\..\Local\Continuum\anaconda3\Scripts\activate.bat modm
+    echo Building...
+    scons program profile=debug
+    echo Done!
+) else if exist %Anaconda%\Scripts\activate.bat (
+    echo Activating Anaconda from overide location %Anaconda%...
+    call %Anaconda%\Scripts\activate.bat modm
+    echo Building...
+    scons program profile=debug
+    echo Done!
+)
+else (
+    echo Anaconda not found, please try again. (Expected location: %AppData%\..\Local\Continuum\anaconda3\Scripts\activate.bat or %Anaconda%\Scripts\activate.bat)
+    exit /B 1
+)
