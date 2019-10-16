bd0acea6a40151e03ea49e01a0c4a59836df01c8
MoSadie <MoSadie@users.noreply.github.com>
2019-10-15 20:51:26 -0700

    Added wait message to Windows scripts

--- scripts/windows/releaseBuild.bat
+++ scripts/windows/releaseBuild.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Building...
+echo Building... (Please wait a minute...)
 call conda run -n modm scons build || (
     echo Something went wrong building, look at the log and try again...
     exit /b 1
--- scripts/windows/setupBuild.bat
+++ scripts/windows/setupBuild.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Running lbuild...
+echo Running lbuild... (Please wait a minute...)
 call conda run -n modm lbuild build || (
     echo Something went wrong while running lbuild, look at the log and try again...
     exit /b 1
