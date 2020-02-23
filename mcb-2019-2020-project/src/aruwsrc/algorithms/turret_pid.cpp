f1a2ec93bd6033408fb0d9ea7b8fbe3f6ac236f0
Matthew Arnold <mma35@uw.edu>
2020-02-23 02:21:25 +0000

    custom pid class

---
+++ mcb-2019-2020-project/src/aruwsrc/algorithms/turret_pid.cpp
@@ -0,0 +1,41 @@
+#include <rm-dev-board-a/board.hpp>
+#include "turret_pid.hpp"
+#include "src/aruwlib/algorithms/math_user_utils.hpp"
+
+using namespace aruwlib::algorithms;
+
+namespace aruwsrc
+{
+
+namespace algorithms
+{
+
+float TurretPid::runController(float error, float errorDerivative)
+{
+    // p
+    currErrorP = kp * proportionalKalman.filterData(error);
+    // i
+    currErrorI = limitVal<float>(currErrorI + ki * proportionalKalman.getLastFiltered(),
+        -maxICumulative, maxICumulative);
+    // d
+    currErrorD = -kd * derivativeKalman.filterData(errorDerivative);
+    // total
+    output = limitVal<float>(currErrorP + currErrorI + currErrorD, -maxOutput, maxOutput);
+    return output;
+}
+
+float TurretPid::runControllerDerivateError(float error, float dt)
+{
+    float errorDerivative = error / dt;
+    previousTimestamp = modm::Clock::now().getTime();
+    return runController(error, errorDerivative);
+}
+
+float TurretPid::getOutput()
+{
+    return output;
+}
+
+}  // namespace algorithms
+
+}  // namespace aruwsrc
---
+++ mcb-2019-2020-project/src/aruwsrc/algorithms/turret_pid.hpp
@@ -0,0 +1,67 @@
+#ifndef __TURRET_PID_HPP__
+#define __TURRET_PID_HPP__
+
+#include "src/aruwlib/algorithms/extended_kalman.hpp"
+
+namespace aruwsrc
+{
+
+namespace algorithms
+{
+
+class TurretPid
+{
+ public:
+    TurretPid(
+        float kp,
+        float ki,
+        float kd,
+        float maxICumulative,
+        float maxOutput,
+        float tQDerivativeKalman,
+        float tRDerivativeKalman,
+        float tQProportionalKalman,
+        float tRProportionalKalman
+    ) :
+    kp(kp),
+    ki(ki),
+    kd(kd),
+    maxICumulative(maxICumulative),
+    maxOutput(maxOutput),
+    proportionalKalman(tQProportionalKalman, tRProportionalKalman),
+    derivativeKalman(tQDerivativeKalman, tRDerivativeKalman)
+    {}
+
+    float runController(float error, float rotationalSpeed);
+
+    float runControllerDerivateError(float error, float dt);
+
+    float getOutput();
+
+ private:
+    // gains and constants, to be set by the user
+    float kp = 0.0f;
+    float ki = 0.0f;
+    float kd = 0.0f;
+    float maxICumulative = 0.0f;
+    float maxOutput = 0.0f;
+
+    // while these could be local, debugging pid is much easier if they are not
+    float currErrorP = 0.0f;
+    float currErrorI = 0.0f;
+    float currErrorD = 0.0f;
+    float output = 0.0f;
+
+    // if you run the controller without inputting a rotationalSpeed, we find the
+    // derivative ourselves
+    uint32_t previousTimestamp = 0;
+
+    aruwlib::algorithms::ExtendedKalman proportionalKalman;
+    aruwlib::algorithms::ExtendedKalman derivativeKalman;
+};
+
+}  // namespace algorithms
+
+}  // namespace aruwsrc
+
+#endif
