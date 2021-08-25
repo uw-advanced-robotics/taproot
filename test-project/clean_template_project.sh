4219d4539e1dfd43fb39fcf54b977607615de271
Matthew Arnold <mma35@uw.edu>
2021-08-25 02:15:49 +0000

    Add script that removes all non-persistent stuff from the test project

---
+++ test-project/clean_template_project.sh
@@ -0,0 +1,32 @@
+# !/bin/bash
+#
+# Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+#
+# This file is part of Taproot.
+#
+# Taproot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Taproot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Taproot.  If not, see <https://www.gnu.org/licenses/>.
+
+# See https://stackoverflow.com/questions/39340169/dir-cd-dirname-bash-source0-pwd-how-does-that-work/39340259
+# for how the following line works.
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR
+
+rm -rf taproot
+rm -rf src/drivers.hpp
+rm -rf build
+rm -f .sconsign.dblite
+rm -f openocd.cfg
+rm -f project.xml.log
+rm -f SConstruct
