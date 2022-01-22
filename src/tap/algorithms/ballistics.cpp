cdc56b8ae3ea942d82984a181f33f580e501b5ed
Manoli P. Tramountanas <manoliptram@gmail.com>
2022-01-22 23:26:27 +0000

    MeasuredKinematicState Rudimentary Implementation
    - Implemented class variables to keep track of the kinematic state of an
    object in C++
    - Implemented all methods to find the ballistics intersection of our
    robot with another object whose kinematic state is known in C++
    
    Next Steps:
    - Fix "divide by 0" issue with 0 pitch angle in
    MeasuredKinematicState::computeTravelTime
    - Write unit tests & make appropriate fixes

--- src/tap/algorithms/ballistics.cpp
+++ src/tap/algorithms/ballistics.cpp
@@ -1,1 +1,60 @@
+/*
+ * Copyright (c) 2020-2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of Taproot.
+ *
+ * Taproot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Taproot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Taproot.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "ballistics.hpp"
+
+namespace tap
+{
+namespace algorithms
+{
+    MeasuredKinematicState::MeasuredKinematicState(
+        modm::Vector<float, 3> position,
+        modm::Vector<float, 3> velocity,
+        modm::Vector<float, 3> acceleration) :
+        position(position),
+        velocity(velocity),
+        acceleration(acceleration)
+    {}                   
+
+    float MeasuredKinematicState::computeTravelTime(modm::Vector<float, 3> targetPosition)
+    {
+        float horizontalDist = hypot(targetPosition[0] - position[0], targetPosition[1] - position[1]);
+        float verticalDist = targetPosition[2] - position[2];
+        float pitchAngle = atan((powf(BULLET_VELOCITY, 2) - sqrt(powf(BULLET_VELOCITY, 4) - G*(G*powf(horizontalDist, 2) + 2*verticalDist*powf(BULLET_VELOCITY, 2))))/(G*horizontalDist));
+
+        return horizontalDist / (BULLET_VELOCITY * cos(pitchAngle));
+    }
+
+    modm::Vector<float, 3> MeasuredKinematicState::findIntersection(MeasuredKinematicState targetInitialState)
+    {
+        modm::Vector<float, 3> projectedTargetPosition = targetInitialState.position;
+        float projectedTravelTime = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            projectedTravelTime = computeTravelTime(projectedTargetPosition);
+            projectedTargetPosition = projectForward(targetInitialState, projectedTravelTime);
+        }
+
+        return projectedTargetPosition;
+    }
+
+} // namespace algorithms
+
+} // namespace tap
--- src/tap/algorithms/ballistics.hpp
+++ src/tap/algorithms/ballistics.hpp
@@ -28,54 +28,61 @@ namespace tap
 namespace algorithms
 {
 
-// struct Vector3D {
-//     float x;
-//     float y;
-//     float z;
-// };
+/**
+ * Stores the 3D position, velocity, and acceleration of an object.
+ * Functionality for finding a position for our robot to aim at a given target
+ * is also built into this class.
+ */
+class MeasuredKinematicState {
+public:
+    /**
+     * @param position in 3D space, measured in meters.
+     * @param velocity in 3D space, measured in m/s.
+     * @param acceleration in 3D space, measured in m/s^2.
+     */
+    MeasuredKinematicState(
+        modm::Vector<float, 3> position,
+        modm::Vector<float, 3> velocity,
+        modm::Vector<float, 3> acceleration);
 
-struct MeasuredKinematicState {
-    modm::Vector<float, 3> position;
-    modm::Vector<float, 3> velocity;
-    modm::Vector<float, 3> acceleration;
+    /**
+     * Given the variables for a 1D kinematic state and an amount of time to project forward,
+     * predicts the future position of an object using a quadratic (constant acceleration) model.
+     */
+    inline float quadraticKinematicProjection(float dt, float s, float v, float a) {
+        return s + v*dt + 0.5f*a*powf(dt, 2.0f);
+    }
+
+    /**
+     * Given the 3D kinematic state of an object and an amount of time to project forward,
+     * predicts the future 3D position of the objust using a quadratic (constant acceleration) model.
+     */
+    inline modm::Vector<float, 3> projectForward(MeasuredKinematicState state, float dt) {
+        return modm::Vector<float, 3>(
+            {quadraticKinematicProjection(dt, state.position[0], state.velocity[0], state.acceleration[0]),
+            quadraticKinematicProjection(dt, state.position[1], state.velocity[1], state.acceleration[1]),
+            quadraticKinematicProjection(dt, state.position[2], state.velocity[2], state.acceleration[2])});
+    }
+
+    /**
+     * Given our position and the position of the target,
+     * finds the expected travel time of a turret shot to hit a target.
+     */
+    float computeTravelTime(modm::Vector<float, 3> targetPosition);
+
+    /**
+     * Given the state of a target that we want to fire at, compute where we should aim to hit that target
+     */
+    modm::Vector<float, 3> findIntersection(MeasuredKinematicState targetInitialState);
+
+private:
+    modm::Vector<float, 3> position; // m
+    modm::Vector<float, 3> velocity; // m/s
+    modm::Vector<float, 3> acceleration; // m/s^2
+
+    static constexpr float G = 9.81; // m/s^2
+    static constexpr float BULLET_VELOCITY = 20; // m/s
 };
-
-// /**
-//  * Doc cmnt
-//  */
-// class MeasuredKinematicState
-// {
-//     public:
-//     MeasuredKinematicState(float x, float y, float z, float v_x, float v_y, float v_z, float a_x, float a_y, float a_z)
-//     : position({x, y, z}),
-//       velocity({v_x, v_y, v_z}),
-//       acceleration({a_x, a_y, a_z}) {};
-
-//     static float quadraticKinematicProjection(float dt, float x_i, float v_i, float a_i) {
-//         return x_i + v_i*dt + 0.5*pow(a_i, 2);
-//     }
-
-//     std::array<float> projectKinematicState(float dt) {
-//         return {quadraticKinematicProjection(dt, position[0], velocity[0], acceleration[0]),
-//                 quadraticKinematicProjection(dt, position[1], velocity[1], acceleration[1]),
-//                 quadraticKinematicProjection(dt, position[2], velocity[2], acceleration[2])};
-//     }
-
-//     private:
-//     float position[3]{};
-//     float velocity[3]{};
-//     float acceleration[3]{};
-// };
-
-inline float quadraticKinematicProjection(float dt, float s, float v, float a) {
-    return s + v*dt + 0.5f*a*powf(dt, 2.0f);
-}
-
-modm::Vector<float, 3> projectForward(MeasuredKinematicState state, float dt) {
-    return modm::Vector<float, 3>({quadraticKinematicProjection(dt, state.position[0], state.velocity[0], state.acceleration[0]),
-                                   quadraticKinematicProjection(dt, state.position[1], state.velocity[1], state.acceleration[1]),
-                                   quadraticKinematicProjection(dt, state.position[2], state.velocity[2], state.acceleration[2])});
-}
 
 }  // namespace algorithms
 
