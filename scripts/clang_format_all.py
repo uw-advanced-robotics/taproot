29e2aaa661689376b4ff67f2d5c9739b239175cf
Matthew Arnold <mma35@uw.edu>
2022-02-01 18:31:58 +0000

    Update scripts/clang_format_all.py

--- scripts/clang_format_all.py
+++ scripts/clang_format_all.py
@@ -1,21 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #
-# Copyright (c) 2020 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+# Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
 #
-# This file is part of taproot.
+# This file is part of Taproot.
 #
-# taproot is free software: you can redistribute it and/or modify
+# Taproot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# taproot is distributed in the hope that it will be useful,
+# Taproot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with taproot.  If not, see <https://www.gnu.org/licenses/>.
+# along with Taproot.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 import subprocess
