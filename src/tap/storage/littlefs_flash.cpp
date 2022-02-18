b96415f591af5328a1fd692fb63572e667a5c4ec
GentlemanXu <xuhongao@yeah.net>
2022-02-17 19:27:13 -0800

    LittleFS integration done

--- src/tap/storage/littlefs_flash.cpp
+++ src/tap/storage/littlefs_flash.cpp
@@ -9,34 +9,22 @@ using namespace modm::platform;
 LittleFSFlash::LittleFSFlash() {
 }
 
-int LittleFSFlash::initialize() {
+void LittleFSFlash::initialize() {
     Flash::enable();
-    for (size_t i = 0; i < 10 && Flash::isLocked(); i++) {
-        Flash::unlock();
-    }
-    if (Flash::isLocked()) {
-        return -1;
-    }
-
-    return 0;
+    Flash::unlock();
 }
 
 int LittleFSFlash::lfs_read(const struct lfs_config *c, lfs_block_t block,
                             lfs_off_t off, void *buffer, lfs_size_t size) {
-    if (block >= c->block_count) {
+    // Offset and size must be aligned
+    if (block >= c->block_count || (size % c->read_size) != 0 || (off % c->read_size) != 0) {
         return LFS_ERR_IO;
     }
 
     uintptr_t startAddr = OriginAddr + block * c->block_size + off;
-    uintptr_t endAddr = OriginAddr + block * c->block_size + off + size;
-    uintptr_t alignedEndAddr = (endAddr >> 2) << 2;
-    uintptr_t alignedSize = (size >> 2) << 2;
 
     for (size_t i = 0; i < size / sizeof(uint32_t); i++) {
         ((uint32_t *)buffer)[i] = *(reinterpret_cast<uint32_t *>(startAddr) + i);
-    }
-    for (size_t i = 0; i < size % sizeof(uint32_t); i++) {
-        ((uint8_t *)buffer)[alignedSize + i] = *(reinterpret_cast<uint8_t *>(alignedEndAddr) + i);
     }
 
     return LFS_ERR_OK;
@@ -44,23 +32,15 @@ int LittleFSFlash::lfs_read(const struct lfs_config *c, lfs_block_t block,
 
 int LittleFSFlash::lfs_program(const struct lfs_config *c, lfs_block_t block,
                                lfs_off_t off, const void *buffer, lfs_size_t size) {
-    if (block >= c->block_count) {
+    // Offset and size must be aligned
+    if (block >= c->block_count || (size % c->prog_size) != 0 || (off % c->prog_size) != 0) {
         return LFS_ERR_IO;
     }
 
     uintptr_t startAddr = OriginAddr + block * c->block_size + off;
-    uintptr_t endAddr = OriginAddr + block * c->block_size + off + size;
-    uintptr_t alignedEndAddr = (endAddr / sizeof(uint32_t)) * sizeof(uint32_t);
-    uintptr_t alignedSize = (size / sizeof(uint32_t)) * sizeof(uint32_t);
 
     for (size_t i = 0; i < size / sizeof(uint32_t); i++) {
         if (Flash::program(startAddr + i * sizeof(uint32_t), ((uint32_t *)buffer)[i]) != 0) {
-            return LFS_ERR_IO;
-        }
-    }
-
-    for (size_t i = 0; i < size % sizeof(uint32_t); i++) {
-        if (Flash::program(alignedEndAddr + i * sizeof(uint8_t), ((uint8_t *)buffer)[alignedSize + i], Flash::WordSize::B8) != 0) {
             return LFS_ERR_IO;
         }
     }
@@ -78,12 +58,4 @@ int LittleFSFlash::lfs_erase(const struct lfs_config *c, lfs_block_t block) {
 
 int LittleFSFlash::lfs_sync(__unused const struct lfs_config *c) {
     return LFS_ERR_OK;
-}
-
-int LittleFSFlash::mount() {
-    return lfs_mount(&fs, &fsconfig);
-}
-
-int LittleFSFlash::format() {
-    return lfs_format(&fs, &fsconfig);
 }
