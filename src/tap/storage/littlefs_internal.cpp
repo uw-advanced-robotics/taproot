06bb52cf38c8c7f72e5e1f1e2801f32b250f3438
GentlemanXu <xuhongao@yeah.net>
2022-03-29 01:58:38 -0700

    Change internal littlefs name

--- src/tap/storage/littlefs_internal.cpp
+++ src/tap/storage/littlefs_internal.cpp
@@ -1,20 +1,19 @@
-#include "littlefs_flash.hpp"
+#include "littlefs_internal.hpp"
 
-#include "modm/architecture/interface/register.hpp"
 #include "modm/platform/flash/flash.hpp"
 
 using namespace tap::storage;
 using namespace modm::platform;
 
-LittleFSFlash::LittleFSFlash() {
+LittleFSInternal::LittleFSInternal() {
 }
 
-void LittleFSFlash::initialize() {
+void LittleFSInternal::initialize() {
     Flash::enable();
     Flash::unlock();
 }
 
-int LittleFSFlash::lfs_read(const struct lfs_config *c, lfs_block_t block,
+int LittleFSInternal::lfs_read(const struct lfs_config *c, lfs_block_t block,
                             lfs_off_t off, void *buffer, lfs_size_t size) {
     // Offset and size must be aligned
     if (block >= c->block_count || (size % c->read_size) != 0 || (off % c->read_size) != 0) {
@@ -30,7 +29,7 @@ int LittleFSFlash::lfs_read(const struct lfs_config *c, lfs_block_t block,
     return LFS_ERR_OK;
 }
 
-int LittleFSFlash::lfs_program(const struct lfs_config *c, lfs_block_t block,
+int LittleFSInternal::lfs_program(const struct lfs_config *c, lfs_block_t block,
                                lfs_off_t off, const void *buffer, lfs_size_t size) {
     // Offset and size must be aligned
     if (block >= c->block_count || (size % c->prog_size) != 0 || (off % c->prog_size) != 0) {
@@ -48,7 +47,7 @@ int LittleFSFlash::lfs_program(const struct lfs_config *c, lfs_block_t block,
     return LFS_ERR_OK;
 }
 
-int LittleFSFlash::lfs_erase(const struct lfs_config *c, lfs_block_t block) {
+int LittleFSInternal::lfs_erase(const struct lfs_config *c, lfs_block_t block) {
     static constexpr int BANK2_INDEX_OFFSET = 4;
     if (block >= c->block_count) {
         return LFS_ERR_IO;
@@ -56,6 +55,6 @@ int LittleFSFlash::lfs_erase(const struct lfs_config *c, lfs_block_t block) {
     return (Flash::erase(SECTOR_ZERO + block + BANK2_INDEX_OFFSET) == 0 ? LFS_ERR_OK : LFS_ERR_IO);
 }
 
-int LittleFSFlash::lfs_sync(__unused const struct lfs_config *c) {
+int LittleFSInternal::lfs_sync(__unused const struct lfs_config *c) {
     return LFS_ERR_OK;
 }
