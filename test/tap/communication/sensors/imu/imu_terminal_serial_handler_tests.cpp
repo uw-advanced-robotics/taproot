d90a1b786e255fe0b8949997e89c45acdffa8f51
Matthew <mma35@uw.edu>
2022-05-11 15:57:57 -0700

    Re-add terminal serial handler tests

--- test/tap/communication/sensors/imu/imu_terminal_serial_handler_tests.cpp
+++ test/tap/communication/sensors/imu/imu_terminal_serial_handler_tests.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ * Copyright (c) 2022 Advanced Robotics at the University of Washington <robomstr@uw.edu>
  *
  * This file is part of Taproot.
  *
@@ -19,60 +19,80 @@
 
 #include <gtest/gtest.h>
 
-#include "tap/communication/sensors/mpu6500/mpu6500_terminal_serial_handler.hpp"
+#include "tap/communication/sensors/imu/imu_terminal_serial_handler.hpp"
 #include "tap/drivers.hpp"
+#include "tap/mock/imu_interface_mock.hpp"
 #include "tap/stub/terminal_device_stub.hpp"
 
 using namespace tap;
-using namespace tap::sensors;
+using namespace tap::mock;
+using namespace tap::communication::sensors::imu;
 using namespace testing;
 
-TEST(Mpu6500TerminalSerialHandler, init__adds_self_to_terminal_serial)
+class ImuTerminalSerialHandlerTest : public Test
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
+protected:
+    ImuTerminalSerialHandlerTest()
+        : serialHandler(&drivers, &imu),
+          terminalDevice(&drivers),
+          stream(terminalDevice)
+    {
+    }
 
+    void SetUp() override { ON_CALL(imu, getName).WillByDefault(Return("imu")); }
+
+    Drivers drivers;
+    NiceMock<ImuInterfaceMock> imu;
+    ImuTerminalSerialHandler serialHandler;
+    tap::stub::TerminalDeviceStub terminalDevice;
+    modm::IOStream stream;
+};
+
+TEST_F(ImuTerminalSerialHandlerTest, init__adds_self_to_terminal_serial)
+{
     EXPECT_CALL(drivers.terminalSerial, addHeader(_, &serialHandler));
 
     serialHandler.init();
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__no_args_returns_false)
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__h_prints_usage)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
+    char input[] = "-h";
+    EXPECT_TRUE(serialHandler.terminalSerialCallback(input, stream, false));
 
-    char input[] = "  \t\n    \r ";
+    EXPECT_THAT(terminalDevice.readAllItemsFromWriteBufferToString(), HasSubstr("Usage"));
+}
+
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__empty_string_input_returns_false)
+{
+    char input[] = "";
     EXPECT_FALSE(serialHandler.terminalSerialCallback(input, stream, false));
 
     EXPECT_THAT(terminalDevice.readAllItemsFromWriteBufferToString(), HasSubstr("Usage"));
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__incorrect_args_returns_false)
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__no_args_returns_false)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
+    char input[] = "  \t\n    \r ";
 
+    EXPECT_FALSE(serialHandler.terminalSerialCallback(input, stream, false));
+
+    EXPECT_THAT(terminalDevice.readAllItemsFromWriteBufferToString(), HasSubstr("Usage"));
+}
+
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__incorrect_args_returns_false)
+{
     char input[] = "anglfes gysros tempehratre";
     EXPECT_FALSE(serialHandler.terminalSerialCallback(input, stream, false));
 
     EXPECT_THAT(terminalDevice.readAllItemsFromWriteBufferToString(), HasSubstr("Usage"));
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__angle_input_return_angle_values)
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__angle_input_return_angle_values)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
-
-    ON_CALL(drivers.mpu6500, getPitch).WillByDefault(Return(12.34));
-    ON_CALL(drivers.mpu6500, getRoll).WillByDefault(Return(90.54));
-    ON_CALL(drivers.mpu6500, getYaw).WillByDefault(Return(180.43));
+    ON_CALL(imu, getPitch).WillByDefault(Return(12.34));
+    ON_CALL(imu, getRoll).WillByDefault(Return(90.54));
+    ON_CALL(imu, getYaw).WillByDefault(Return(180.43));
 
     char input[] = "angle";
     EXPECT_TRUE(serialHandler.terminalSerialCallback(input, stream, false));
@@ -86,16 +106,11 @@ TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__angle_input_return_an
     EXPECT_THAT(output, HasSubstr("180.43"));
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__gyro_input_return_gyro_values)
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__gyro_input_return_gyro_values)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
-
-    ON_CALL(drivers.mpu6500, getGx).WillByDefault(Return(12.34));
-    ON_CALL(drivers.mpu6500, getGy).WillByDefault(Return(90.54));
-    ON_CALL(drivers.mpu6500, getGz).WillByDefault(Return(180.43));
+    ON_CALL(imu, getGx).WillByDefault(Return(12.34));
+    ON_CALL(imu, getGy).WillByDefault(Return(90.54));
+    ON_CALL(imu, getGz).WillByDefault(Return(180.43));
 
     char input[] = "gyro";
     EXPECT_TRUE(serialHandler.terminalSerialCallback(input, stream, false));
@@ -109,16 +124,11 @@ TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__gyro_input_return_gyr
     EXPECT_THAT(output, HasSubstr("180.43"));
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__accel_input_return_accel_value)
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__accel_input_return_accel_value)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
-
-    ON_CALL(drivers.mpu6500, getAx).WillByDefault(Return(12.34));
-    ON_CALL(drivers.mpu6500, getAy).WillByDefault(Return(90.54));
-    ON_CALL(drivers.mpu6500, getAz).WillByDefault(Return(180.43));
+    ON_CALL(imu, getAx).WillByDefault(Return(12.34));
+    ON_CALL(imu, getAy).WillByDefault(Return(90.54));
+    ON_CALL(imu, getAz).WillByDefault(Return(180.43));
 
     char input[] = "accel";
     EXPECT_TRUE(serialHandler.terminalSerialCallback(input, stream, false));
@@ -132,14 +142,9 @@ TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__accel_input_return_ac
     EXPECT_THAT(output, HasSubstr("180.43"));
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__temp_input_return_temp_value)
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__temp_input_return_temp_value)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
-
-    ON_CALL(drivers.mpu6500, getTemp).WillByDefault(Return(32.42));
+    ON_CALL(imu, getTemp).WillByDefault(Return(32.42));
 
     char input[] = "temp";
     EXPECT_TRUE(serialHandler.terminalSerialCallback(input, stream, false));
@@ -149,23 +154,20 @@ TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__temp_input_return_tem
     EXPECT_THAT(output, HasSubstr("32.42"));
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__temp_gyro_angle_accel_return_all_values)
+TEST_F(
+    ImuTerminalSerialHandlerTest,
+    terminalSerialCallback__temp_gyro_angle_accel_return_all_values)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
-
-    ON_CALL(drivers.mpu6500, getPitch).WillByDefault(Return(123.54));
-    ON_CALL(drivers.mpu6500, getRoll).WillByDefault(Return(-42.05));
-    ON_CALL(drivers.mpu6500, getYaw).WillByDefault(Return(1.89));
-    ON_CALL(drivers.mpu6500, getGx).WillByDefault(Return(12.34));
-    ON_CALL(drivers.mpu6500, getGy).WillByDefault(Return(-12.53));
-    ON_CALL(drivers.mpu6500, getGz).WillByDefault(Return(-54.01));
-    ON_CALL(drivers.mpu6500, getAx).WillByDefault(Return(12.34));
-    ON_CALL(drivers.mpu6500, getAy).WillByDefault(Return(90.54));
-    ON_CALL(drivers.mpu6500, getAz).WillByDefault(Return(180.43));
-    ON_CALL(drivers.mpu6500, getTemp).WillByDefault(Return(32.42));
+    ON_CALL(imu, getPitch).WillByDefault(Return(123.54));
+    ON_CALL(imu, getRoll).WillByDefault(Return(-42.05));
+    ON_CALL(imu, getYaw).WillByDefault(Return(1.89));
+    ON_CALL(imu, getGx).WillByDefault(Return(12.34));
+    ON_CALL(imu, getGy).WillByDefault(Return(-12.53));
+    ON_CALL(imu, getGz).WillByDefault(Return(-54.01));
+    ON_CALL(imu, getAx).WillByDefault(Return(12.34));
+    ON_CALL(imu, getAy).WillByDefault(Return(90.54));
+    ON_CALL(imu, getAz).WillByDefault(Return(180.43));
+    ON_CALL(imu, getTemp).WillByDefault(Return(32.42));
 
     char input[] = "temp gyro angle accel";
     EXPECT_TRUE(serialHandler.terminalSerialCallback(input, stream, false));
@@ -195,29 +197,19 @@ TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__temp_gyro_angle_accel
     EXPECT_THAT(output, HasSubstr("32.42"));
 }
 
-TEST(Mpu6500TerminalSerialHandler, terminalSerialCallback__temp_with_bad_input_return_false)
+TEST_F(ImuTerminalSerialHandlerTest, terminalSerialCallback__temp_with_bad_input_return_false)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
-
     char input[] = "temp ldskf";
     EXPECT_FALSE(serialHandler.terminalSerialCallback(input, stream, false));
 
     EXPECT_THAT(terminalDevice.readAllItemsFromWriteBufferToString(), HasSubstr("Usage"));
 }
 
-TEST(
-    Mpu6500TerminalSerialHandler,
+TEST_F(
+    ImuTerminalSerialHandlerTest,
     terminalSerialCallback__streaming_mode_with_temp_displays_temp_over_and_over)
 {
-    Drivers drivers;
-    Mpu6500TerminalSerialHandler serialHandler(&drivers);
-    tap::stub::TerminalDeviceStub terminalDevice(&drivers);
-    modm::IOStream stream(terminalDevice);
-
-    ON_CALL(drivers.mpu6500, getTemp).WillByDefault(Return(32.42));
+    ON_CALL(imu, getTemp).WillByDefault(Return(32.42));
 
     char input[] = "temp";
     EXPECT_TRUE(serialHandler.terminalSerialCallback(input, stream, true));
@@ -227,7 +219,7 @@ TEST(
     EXPECT_THAT(output, HasSubstr("temp"));
     EXPECT_THAT(output, HasSubstr("32.42"));
 
-    ON_CALL(drivers.mpu6500, getTemp).WillByDefault(Return(12.31));
+    ON_CALL(imu, getTemp).WillByDefault(Return(12.31));
 
     serialHandler.terminalSerialStreamCallback(stream);
 
@@ -235,7 +227,7 @@ TEST(
     EXPECT_THAT(output, Not(HasSubstr("temp")));
     EXPECT_THAT(output, HasSubstr("12.31"));
 
-    ON_CALL(drivers.mpu6500, getTemp).WillByDefault(Return(54.42));
+    ON_CALL(imu, getTemp).WillByDefault(Return(54.42));
 
     serialHandler.terminalSerialStreamCallback(stream);
 
