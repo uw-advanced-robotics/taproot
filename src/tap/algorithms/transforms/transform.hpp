159f59f8cdc16d064bbb1f7faf12ddf894925ae7
Joshua Tran <profjat@uw.edu>
2023-04-01 20:38:40 +0000

    Added default constructor
    Co-authored-by: ProfJAT <JoshuaT22@lakesideschool.org>

--- src/tap/algorithms/transforms/transform.hpp
+++ src/tap/algorithms/transforms/transform.hpp
@@ -72,6 +72,11 @@ public:
     Transform(float x, float y, float z, float A, float B, float C);
 
     /**
+     * Constructs a new Transform, which represents a transformation between two frames.
+     */
+    Transform();
+
+    /**
      * Returns the composed transformation of the given transformations.
      *
      * @param source Transformation from frame A to frame B.
