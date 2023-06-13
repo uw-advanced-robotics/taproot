41306f2a2a558604221cd3b9a8cd25e8952f2b19
manolipt <manolipt@proton.me>
2023-06-12 18:53:16 -0700

    Remove extra refreshSafeDisconnect() call

--- src/tap/control/command_scheduler.cpp
+++ src/tap/control/command_scheduler.cpp
@@ -185,12 +185,6 @@ void CommandScheduler::run()
 
     if (safeDisconnected())
     {
-        // Tells subsystems in the registeredSubsystemBitmap to shut off their hardware
-        for (auto it = subMapBegin(); it != subMapEnd(); it++)
-        {
-            (*it)->refreshSafeDisconnect();
-        }
-
         // End all commands running. They were interrupted by the remote disconnecting.
         for (auto it = cmdMapBegin(); it != cmdMapEnd(); it++)
         {
@@ -216,10 +210,13 @@ void CommandScheduler::run()
         // Refresh subsystems in the registeredSubsystemBitmap
         for (auto it = subMapBegin(); it != subMapEnd(); it++)
         {
+            // If in safe disconnect,
+            // tell subsystems in the registeredSubsystemBitmap to shut off their hardware
             if (safeDisconnected())
             {
                 (*it)->refreshSafeDisconnect();
             }
+            // Otherwise, refresh the subsystem as usual
             else
             {
                 (*it)->refresh();
