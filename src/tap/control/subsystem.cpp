cb1a4e42a7136892341a9e469f8d3b44ee9e6762
RacketeerGames <zhy0224@gmail.com>
2023-11-21 17:17:40 -0800

    More Clang Format

--- src/tap/control/subsystem.cpp
+++ src/tap/control/subsystem.cpp
@@ -18,7 +18,9 @@
  */
 
 #include "subsystem.hpp"
+
 #include "tap/drivers.hpp"
+
 #include "command_scheduler.hpp"
 
 namespace tap
@@ -34,7 +36,8 @@ Subsystem::Subsystem(Drivers* drivers)
 
 Subsystem::~Subsystem() { CommandScheduler::destructSubsystem(this); }
 
-void Subsystem::registerAndInitialize(){
+void Subsystem::registerAndInitialize()
+{
     initialize();
     drivers->commandScheduler.registerSubsystem(this);
 }
