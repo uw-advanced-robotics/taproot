025ab3bae4380ec43ddf3cfd6f872d841168b1f2
Sumedh P <usa1998dec@gmail.com>
2024-03-27 15:52:45 +0000

    Changed variable naming

--- src/tap/motor/dji_motor.hpp
+++ src/tap/motor/dji_motor.hpp
@@ -75,9 +75,10 @@ class DjiMotor : public can::CanRxListener, public MotorInterface
 public:
     // 0 - 8191 for dji motors
     static constexpr uint16_t ENC_RESOLUTION = 8192;
+
     // Maximum values for following motors
-    static constexpr uint16_t C620 = 16384;
-    static constexpr uint16_t GM6020 = 25000;
+    static constexpr uint16_t MAX_OUTPUT_C620 = 16384;
+    static constexpr uint16_t MAX_OUTPUT_GM6020 = 25000;
 
     /**
      * @param drivers a pointer to the drivers struct
