f2c47054f2712bbfb06e0f2c3b7625985ec620de
Sumedh <sumedh@uw.edu>
2024-04-13 18:18:04 -0700

    Fix some of the comments

--- src/tap/motor/dji_motor.hpp
+++ src/tap/motor/dji_motor.hpp
@@ -77,14 +77,17 @@ public:
     static constexpr uint16_t ENC_RESOLUTION = 8192;
 
     // Maximum values for following motors
-    static constexpr uint16_t MAX_OUTPUT_C610 = 10000;  // Controller for the M2006, in mA output
-    static constexpr uint16_t MAX_OUTPUT_C620 = 16384;  // Controller for the M3508, in mA output
-    static constexpr uint16_t MAX_OUTPUT_820R =
-        32767;  // Controller for the M3510 (I think I'm being lied to by the spec sheet, the max
-                // current for a M3510 is 20A. I think its really 16384)
+    // Controller for the M2006, in mA output
+    static constexpr uint16_t MAX_OUTPUT_C610 = 10000;
+    // Controller for the M3508, in mA output (Mapped to a 20A range though, not 1:1)
+    static constexpr uint16_t MAX_OUTPUT_C620 = 16384;
+    // Controller for the M3510, in mA output (Not known if mapped to 20A range)
+    static constexpr uint16_t MAX_OUTPUT_820R = 32767;
 
-    static constexpr uint16_t MAX_OUTPUT_GM6020 = 25000;  // Output is in mV
-    static constexpr uint16_t MAX_OUTPUT_GM3510 = 29000;  // Output is in mV
+    // Output is in mV
+    static constexpr uint16_t MAX_OUTPUT_GM6020 = 25000;
+    // Output is in mV
+    static constexpr uint16_t MAX_OUTPUT_GM3510 = 29000;
 
     // Internal gear ratio of the following motors
     static constexpr float GEAR_RATIO_M3508 = 3591.0f / 187.0f;
