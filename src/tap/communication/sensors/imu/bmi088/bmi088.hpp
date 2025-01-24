441fa5a89d5ef14782c5fde59980309403041a87
Clementine Allen <callen42@uw.edu>
2025-01-23 16:35:41 -0800

    accel sens method

--- src/tap/communication/sensors/imu/bmi088/bmi088.hpp
+++ src/tap/communication/sensors/imu/bmi088/bmi088.hpp
@@ -112,7 +112,12 @@ public:
         imuHeater.setDesiredTemperature(temperatureC);
     }
 
+
+
 private:
+
+    inline float getAccelerationSensitivity() override { return ACC_G_PER_ACC_COUNT; }
+
     static constexpr uint16_t RAW_TEMPERATURE_TO_APPLY_OFFSET = 1023;
     /// Offset parsed temperature reading by this amount if > RAW_TEMPERATURE_TO_APPLY_OFFSET.
     static constexpr int16_t RAW_TEMPERATURE_OFFSET = -2048;
