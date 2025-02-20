02a6090272eaa492b07dab9c685f416d705c0ab5
Eli Orona <eliorona@live.com>
2025-02-20 15:03:51 -0800

    Clang

--- src/tap/algorithms/cmsis_mat.hpp
+++ src/tap/algorithms/cmsis_mat.hpp
@@ -145,7 +145,7 @@ struct CMSISMat
                 ret.data[row * ROWS + col] /= temp;
             }
 
-            for (int otherRow = 0; otherRow < ROWS; otherRow ++)
+            for (int otherRow = 0; otherRow < ROWS; otherRow++)
             {
                 if (row == otherRow) continue;
                 temp = modified.data[otherRow * ROWS + row];
@@ -158,18 +158,18 @@ struct CMSISMat
         }
 
         // for i_maj in range(modify.shape[0]):
-            // temp = modify[i_maj, i_maj]
-            // for j in range(modify.shape[1]):
-            //     modify[i_maj, j] /= temp
-            //     ret[i_maj, j] /= temp
+        // temp = modify[i_maj, i_maj]
+        // for j in range(modify.shape[1]):
+        //     modify[i_maj, j] /= temp
+        //     ret[i_maj, j] /= temp
 
-            // for i in range(modify.shape[0]):
-            //     if i == i_maj:
-            //         continue
-            //     temp = modify[i, i_maj]
-            //     for j in range(modify.shape[1]):
-            //         modify[i, j] -= temp * modify[i_maj, j]
-            //         ret[i, j] -= temp * ret[i_maj, j]
+        // for i in range(modify.shape[0]):
+        //     if i == i_maj:
+        //         continue
+        //     temp = modify[i, i_maj]
+        //     for j in range(modify.shape[1]):
+        //         modify[i, j] -= temp * modify[i_maj, j]
+        //         ret[i, j] -= temp * ret[i_maj, j]
 #endif
         return ret;
     }
@@ -180,9 +180,9 @@ struct CMSISMat
 #ifndef PLATFORM_HOSTED
         assert(ARM_MATH_SUCCESS == arm_mat_trans_f32(&this->matrix, &ret.matrix));
 #else
-        for(int i = 0; i < ROWS; i++)
+        for (int i = 0; i < ROWS; i++)
         {
-            for (int j = 0; j < COLS; j ++)
+            for (int j = 0; j < COLS; j++)
             {
                 ret.data[i * ROWS + j] = data[j * ROWS + i];
             }
@@ -267,7 +267,7 @@ inline CMSISMat<ROWS, COLS> operator/(const CMSISMat<ROWS, COLS> &a, float b)
     assert(ARM_MATH_SUCCESS == arm_mat_scale_f32(&a.matrix, b, &c.matrix));
     return c;
 }
-#else 
+#else
 template <uint16_t A_ROWS, uint16_t A_COLS, uint16_t B_ROWS, uint16_t B_COLS>
 inline CMSISMat<A_ROWS, A_COLS> operator+(
     const CMSISMat<A_ROWS, A_COLS> &a,
@@ -278,8 +278,8 @@ inline CMSISMat<A_ROWS, A_COLS> operator+(
         "Invalid size of CMSISMat matricies in operator+");
 
     CMSISMat<A_ROWS, A_COLS> c;
-    
-    for(int i = 0; i < A_ROWS * A_COLS; i++)
+
+    for (int i = 0; i < A_ROWS * A_COLS; i++)
     {
         c.data[i] = a.data[i] + b.data[i];
     }
@@ -298,11 +298,11 @@ inline CMSISMat<A_ROWS, A_COLS> operator-(
 
     CMSISMat<A_ROWS, A_COLS> c;
 
-    for(int i = 0; i < A_ROWS * A_COLS; i++)
+    for (int i = 0; i < A_ROWS * A_COLS; i++)
     {
         c.data[i] = a.data[i] - b.data[i];
     }
-    
+
     return c;
 }
 
@@ -312,7 +312,7 @@ inline CMSISMat<ROWS, COLS> operator-(const CMSISMat<ROWS, COLS> &a)
     float scale(-1);
     CMSISMat<ROWS, COLS> c;
 
-    for(int i = 0; i < ROWS * COLS; i++)
+    for (int i = 0; i < ROWS * COLS; i++)
     {
         c.data[i] = scale * a.data[i];
     }
@@ -327,13 +327,16 @@ inline CMSISMat<A_ROWS, B_COLS> operator*(
 {
     static_assert(A_COLS == B_ROWS, "Invalid size of CMSISMat matricies in operator*");
 
-    CMSISMat<A_ROWS, B_COLS> c; // Result matrix
+    CMSISMat<A_ROWS, B_COLS> c;  // Result matrix
 
-    for (uint16_t i = 0; i < A_ROWS; i++) {
-        for (uint16_t j = 0; j < B_COLS; j++) {
+    for (uint16_t i = 0; i < A_ROWS; i++)
+    {
+        for (uint16_t j = 0; j < B_COLS; j++)
+        {
             c.data[i * B_COLS + j] = 0;
 
-            for (uint16_t k = 0; k < A_COLS; k++) {
+            for (uint16_t k = 0; k < A_COLS; k++)
+            {
                 c.data[i * B_COLS + j] += a.data[i * A_COLS + k] * b.data[k * B_COLS + j];
             }
         }
@@ -346,8 +349,8 @@ template <uint16_t ROWS, uint16_t COLS>
 inline CMSISMat<ROWS, COLS> operator*(const CMSISMat<ROWS, COLS> &a, float b)
 {
     CMSISMat<ROWS, COLS> c;
-    
-    for(int i = 0; i < ROWS * COLS; i++)
+
+    for (int i = 0; i < ROWS * COLS; i++)
     {
         c.data[i] = b * a.data[i];
     }
@@ -360,7 +363,7 @@ inline CMSISMat<ROWS, COLS> operator*(float b, const CMSISMat<ROWS, COLS> &a)
 {
     CMSISMat<ROWS, COLS> c;
 
-    for(int i = 0; i < ROWS * COLS; i++)
+    for (int i = 0; i < ROWS * COLS; i++)
     {
         c.data[i] = b * a.data[i];
     }
@@ -373,8 +376,8 @@ inline CMSISMat<ROWS, COLS> operator/(const CMSISMat<ROWS, COLS> &a, float b)
 {
     b = 1 / b;
     CMSISMat<ROWS, COLS> c;
-    
-    for(int i = 0; i < ROWS * COLS; i++)
+
+    for (int i = 0; i < ROWS * COLS; i++)
     {
         c.data[i] = b * a.data[i];
     }
