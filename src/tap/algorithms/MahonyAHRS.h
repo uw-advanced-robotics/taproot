84c46e5dfa3590b0814f5fe914cb23cc925c885f
pie474 <chinmaymurthy474@gmail.com>
2025-03-05 21:30:03 -0800

    deprecate degrees getters

--- src/tap/algorithms/MahonyAHRS.h
+++ src/tap/algorithms/MahonyAHRS.h
@@ -70,8 +70,20 @@ public:
         float my,
         float mz);
     void updateIMU(float gx, float gy, float gz, float ax, float ay, float az);
-    float getRoll() const { return roll * 57.29578f; }
-    float getPitch() const { return pitch * 57.29578f; }
+
+    [[deprecated]]
+    float getRoll() const
+    {
+        return roll * 57.29578f;
+    }
+
+    [[deprecated]]
+    float getPitch() const
+    {
+        return pitch * 57.29578f;
+    }
+
+    [[deprecated]]
     float getYaw() const
     {
         float yawDegrees = yaw * 57.29578f;
