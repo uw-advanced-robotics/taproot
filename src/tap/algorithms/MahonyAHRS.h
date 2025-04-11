3cc4b5be1e47686e3ea1a9c717de67ddd9812eae
pie474 <chinmaymurthy474@gmail.com>
2025-04-11 00:32:12 -0700

    hardcode 2pi bc it doesnt exist yet when mahony is compiled?

--- src/tap/algorithms/MahonyAHRS.h
+++ src/tap/algorithms/MahonyAHRS.h
@@ -74,7 +74,7 @@ public:
     void updateIMU(float gx, float gy, float gz, float ax, float ay, float az);
     float getRoll() const { return roll; }
     float getPitch() const { return pitch; }
-    float getYaw() const { return fmod(yaw + 2 * M_PI, 2 * M_PI); }
+    float getYaw() const { return fmod(yaw + 6.28318530718, 6.28318530718); }
     float getRollRadians() const { return roll; }
     float getPitchRadians() const { return pitch; }
     float getYawRadians() const { return yaw; }
