0ed8121dc01f7a884be300623805048579559a57
pie474 <chinmaymurthy474@gmail.com>
2025-04-14 18:18:27 -0700

    LSB_PER_RAD_PER_S

--- src/tap/communication/sensors/imu/mpu6500/mpu6500.cpp
+++ src/tap/communication/sensors/imu/mpu6500/mpu6500.cpp
@@ -135,8 +135,7 @@ bool Mpu6500::read()
         imuData.accG =
             (imuData.accRaw - imuData.accOffsetRaw) * GRAVITY_MPS2 / ACCELERATION_SENSITIVITY;
 
-        imuData.gyroRadPerSec =
-            (imuData.gyroRaw - imuData.gyroOffsetRaw) / LSB_D_PER_S_TO_RAD_PER_S;
+        imuData.gyroRadPerSec = (imuData.gyroRaw - imuData.gyroOffsetRaw) / LSB_PER_RAD_PER_S;
 
         imuData.temperature = parseTemp(static_cast<float>(rxBuff[6] << 8 | rxBuff[7]));
 
