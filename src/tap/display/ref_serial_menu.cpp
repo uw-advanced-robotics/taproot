f977be261c388813333d6824c5968039898f70c2
Eli Orona <eliorona@live.com>
2025-04-16 14:45:46 -0700

    Fix tests and format

--- src/tap/display/ref_serial_menu.cpp
+++ src/tap/display/ref_serial_menu.cpp
@@ -121,9 +121,7 @@ void RefSerialMenu::printPowerBuf(modm::IOStream& stream)
 void RefSerialMenu::printPower(modm::IOStream& stream)
 {
     const auto& robotData = drivers->refSerial.getRobotData();
-    stream.printf(
-        "Power limit: %i",
-        robotData.chassis.powerConsumptionLimit);
+    stream.printf("Power limit: %i", robotData.chassis.powerConsumptionLimit);
 }
 
 }  // namespace tap::display
