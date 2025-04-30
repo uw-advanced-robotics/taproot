5f45fc34ffd08981dd13d8e927b64761f1fe9203
Joshua Zhang <zhy0224@gmail.com>
2025-04-29 20:27:29 -0700

    Use const flaot instead of float

--- src/tap/algorithms/odometry/odometry_2d_tracker.cpp
+++ src/tap/algorithms/odometry/odometry_2d_tracker.cpp
@@ -77,7 +77,7 @@ void Odometry2DTracker::update()
     }
 }
 
-void Odometry2DTracker::overrideOdometryPosition(float positionX, float positionY)
+void Odometry2DTracker::overrideOdometryPosition(const float positionX, const float positionY)
 {
     location.setPosition(positionX, positionY);
 }
--- src/tap/algorithms/odometry/odometry_2d_tracker.hpp
+++ src/tap/algorithms/odometry/odometry_2d_tracker.hpp
@@ -75,7 +75,7 @@ public:
 
     inline uint32_t getLastComputedOdometryTime() const final { return lastComputedOdometryTime; }
 
-    void overrideOdometryPosition(float positionX, float positionY);
+    void overrideOdometryPosition(const float positionX, const float positionY);
 
 private:
     ChassisWorldYawObserverInterface* chassisYawObserver;
