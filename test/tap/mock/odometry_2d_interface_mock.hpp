221076e57fdf792babd2512d51c6507d344a588e
ananyaa06 <ananyaa06@gmail.com>
2025-05-22 16:29:01 -0700

    clang formatting

--- test/tap/mock/odometry_2d_interface_mock.hpp
+++ test/tap/mock/odometry_2d_interface_mock.hpp
@@ -38,12 +38,7 @@ public:
         overrideOdometryPosition,
         (const float positionX, const float positionY),
         (override));
-    MOCK_METHOD(
-        void,
-        reset,
-        (),
-        (override)
-    );
+    MOCK_METHOD(void, reset, (), (override));
 };
 }  // namespace tap::mock
 
