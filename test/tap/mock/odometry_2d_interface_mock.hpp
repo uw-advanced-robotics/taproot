6862c7994486cb9e804ac8762e3e914f7e77402b
ananyaa06 <ananyaa06@gmail.com>
2025-05-22 16:22:39 -0700

    Adding reset function to odometry 2d interface

--- test/tap/mock/odometry_2d_interface_mock.hpp
+++ test/tap/mock/odometry_2d_interface_mock.hpp
@@ -38,6 +38,12 @@ public:
         overrideOdometryPosition,
         (const float positionX, const float positionY),
         (override));
+    MOCK_METHOD(
+        void,
+        reset,
+        (),
+        (override)
+    );
 };
 }  // namespace tap::mock
 
