147d4185bf0167fd424f7cfa6dc7280c5d2f2a94
Eli Orona <eliorona@live.com>
2025-07-16 15:21:26 -0700

    Fix pipeline?

--- gitlab-ci-scripts/collect_modules.py
+++ gitlab-ci-scripts/collect_modules.py
@@ -12,7 +12,10 @@ def collect_generation_information() -> Dict[str, Dict[str, Iterable[str]]]:
     generation_information = {}
     lbuild_discover = lambda args=[]: subprocess.run(['lbuild', *args, 'discover'], env={**dict(os.environ), **{"PYTHONIOENCODING": "utf-8"}}, shell=True, capture_output=True)
 
-    devices = re.findall(b'Option\(dev_board\) = .* in \\[(.*)\\]', lbuild_discover().stdout)[0].decode("utf-8").split(", ")
+    device_discover = lbuild_discover()
+    print(device_discover.stdout)
+    print(device_discover.stderr)
+    devices = re.findall(b'Option\(dev_board\) = .* in \\[(.*)\\]', device_discover.stdout)[0].decode("utf-8").split(", ")
     print(f"Found {len(devices)} devices: {devices}")
 
     jobs = 0
