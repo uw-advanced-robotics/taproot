40dba6fed1a8169d6f4fc9d965eed288f3e86aea
Eli Orona <eliorona@live.com>
2025-07-18 00:13:15 -0700

    Finished pipeline?

--- gitlab-ci-scripts/project_xml_generation.py
+++ gitlab-ci-scripts/project_xml_generation.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import itertools
 from typing import Iterable, Dict
 
 PROJECT_XML_TEMPLATE_PATH = "ci-project-template.xml"
@@ -23,3 +25,19 @@ def generate_project_xml(board: str, options: Dict[str, bool], modules: Iterable
             out.write(file)
     
     return output
+
+
+if __name__ == "__main__":
+    args = list(sys.argv)
+    _, device, modules, *_ = args
+
+    modules = modules.split(",")
+
+    options = {}
+    if len(args) == 4:
+        options = {
+            opt.split("=")[0]: opt.split("=")[1] == "True" for opt in args[3].split(",")
+        }
+    
+    xml = generate_project_xml(device, options, modules)
+    print(xml)
