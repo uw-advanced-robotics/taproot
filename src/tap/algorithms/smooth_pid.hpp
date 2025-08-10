c361a380f29a84773044a36e901ae7016aeb9d07
AidenP <aprevey@uw.edu>
2025-08-09 21:00:58 -0700

    DAMN YOU CLANG!!

--- src/tap/algorithms/smooth_pid.hpp
+++ src/tap/algorithms/smooth_pid.hpp
@@ -47,7 +47,7 @@ struct SmoothPidConfig
                                         * error will be set to 0. */
     float errorDerivativeFloor = 0.0f; /**< Minimum error value at which the PID controller will
                                         * compute and apply the derivative term. */
-    bool smoothDeadzone = false;          /**< If true, the error will be smoothed by subtracting
+    bool smoothDeadzone = false;       /**< If true, the error will be smoothed by subtracting
                                         * errDeadzone from positive errors and adding it to
                                         * negative errors. */
     bool antiSaturation = false;       /**< If true the controller will not
