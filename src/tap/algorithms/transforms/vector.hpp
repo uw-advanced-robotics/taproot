971409cd4952dead725029b0ba890ba6df52767a
Aiden Joon Ho Prevey <aprevey@uw.edu>
2025-11-23 11:19:32 +0000

    Update file vector.hpp

--- src/tap/algorithms/transforms/vector.hpp
+++ src/tap/algorithms/transforms/vector.hpp
@@ -95,7 +95,7 @@ public:
 
     inline Vector normalize() const { return (*this) * 1.0f / this->magnitude(); };
 
-    inline static Vector normalize(const Vector& a) const { return a * 1.0f / a.magnitude(); };
+    inline static Vector normalize(const Vector& a) { return a * 1.0f / a.magnitude(); };
 
     friend class Transform;
     friend class DynamicPosition;
