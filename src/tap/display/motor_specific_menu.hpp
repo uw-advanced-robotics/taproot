676d8c61f91542e65552bdc604774080b10df34e
Aiden Joon Ho Prevey <aprevey@uw.edu>
2026-02-28 21:36:07 +0000

    Adding ability to see if motors have gone offline since past reset call
    Co-authored-by: swaras <swaras@uw.edu>
    Co-authored-by: AidenPrevey <aidenprevey@gmail.com>

--- src/tap/display/motor_specific_menu.hpp
+++ src/tap/display/motor_specific_menu.hpp
@@ -21,6 +21,7 @@
 #define TAPROOT_MOTOR_SPECIFIC_MENU_HPP_
 
 #include "tap/architecture/periodic_timer.hpp"
+#include "tap/errors/create_errors.hpp"
 
 #include "modm/ui/menu/abstract_menu.hpp"
 
@@ -64,6 +65,11 @@ private:
     bool currIsInverted = false;
     uint16_t currEncoderWrapped;
     int16_t currRPM = 0;
+    bool hasMotorBeenOffline = false;
+    bool motorWasOnline = false;
+
+    // helper function that gets the mutable motor and resets its changed flag
+    void resetMotorChangedFlag();
 };
 }  // namespace display
 }  // namespace tap
