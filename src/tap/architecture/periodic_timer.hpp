99098b1a74caf46783c4079051cadbb23cbb3efb
Clementine Allen <callen42@uw.edu>
2026-03-21 20:27:16 -0700

    Fix

--- src/tap/architecture/periodic_timer.hpp
+++ src/tap/architecture/periodic_timer.hpp
@@ -83,6 +83,10 @@ public:
                 timeout.expireTime += period;
             } while (timeout.expireTime <= now);
 
+            if (timeout.expireTime >= UINT32_MAX - period) {
+                timeout.expireTime = now + period;
+            }
+
             timeout.isRunning = true;
             timeout.isExecuted = false;
             return true;
