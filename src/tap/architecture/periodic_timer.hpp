916d2c6b71d53df7d43bccae65cc7fd13efd96df
Clementine Allen <callen42@uw.edu>
2026-03-22 17:56:50 -0700

    erm what the sigmas

--- src/tap/architecture/periodic_timer.hpp
+++ src/tap/architecture/periodic_timer.hpp
@@ -74,6 +74,9 @@ public:
      */
     inline bool execute()
     {
+        if (timeout.expireTime >= UINT32_MAX - period) {
+                timeout.expireTime = now + period;
+        }
         if (timeout.execute())
         {
             uint32_t now = T::TimeFunc();
@@ -82,10 +85,6 @@ public:
             {
                 timeout.expireTime += period;
             } while (timeout.expireTime <= now);
-
-            if (timeout.expireTime >= UINT32_MAX - period) {
-                timeout.expireTime = now + period;
-            }
 
             timeout.isRunning = true;
             timeout.isExecuted = false;
