c36125098309b516187f1505306a11068eb3f482
Sumedh P <usa1998dec@gmail.com>
2026-05-01 19:55:10 +0000

    Ref system custom controller data
    Co-authored-by: AidenP <aprevey@uw.edu>
    Co-authored-by: SumedhP <43325361+SumedhP@users.noreply.github.com>
    Co-authored-by: Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
    Co-authored-by: Sumedh <sumedh@uw.edu>

--- src/tap/communication/serial/ref_serial.hpp
+++ src/tap/communication/serial/ref_serial.hpp
@@ -105,7 +105,7 @@ public:
         REF_MESSAGE_TYPE_RADAR_INFO = 0x20E,
 
         REF_MESSAGE_TYPE_CUSTOM_DATA = 0x301,
-        // REF_MESSAGE_TYPE_CUSTOM_CONTROLLER_DATA_RECEIVE = 0x302,
+        REF_MESSAGE_TYPE_CUSTOM_CONTROLLER_DATA_RECEIVE = 0x302,
         // REF_MESSAGE_TYPE_SMALL_MAP_INTERACTION = 0x303,
         // REF_MESSAGE_TYPE_VTM_INPUT_DATA = 0x304,
         // REF_MESSAGE_TYPE_RADAR_MINIMAP = 0x305,
@@ -292,6 +292,10 @@ private:
      * Decodes ref serial message containing information about the radar station's actions.
      */
     bool decodeToRadarInfo(const ReceivedSerialMessage& message);
+    /**
+     * Decodes ref serial message containing custom controller data.
+     */
+    bool decodeToCustomControllerData(const ReceivedSerialMessage& message);
 
     bool handleRobotToRobotCommunication(const ReceivedSerialMessage& message);
 
