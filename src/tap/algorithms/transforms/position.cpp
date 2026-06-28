0a82005521df0b56975b7d9e6fb53f2d9731ca86
Chinmay Murthy <cmurthy@uw.edu>
2026-06-28 17:55:46 +0000

    More convenience methods/constructors for the Transforms system
    Co-authored-by: pie474 <chinmaymurthy474@gmail.com>

--- src/tap/algorithms/transforms/position.cpp
+++ src/tap/algorithms/transforms/position.cpp
@@ -23,6 +23,8 @@
 
 namespace tap::algorithms::transforms
 {
+Vector Position::toVector() const { return Vector(this->coordinates_); }
+
 Position Position::operator-(const Vector& other) const
 {
     return Position(this->coordinates_ - other.coordinates());
@@ -38,20 +40,14 @@ Position Position::operator+(const Vector& vector) const
     return Position(this->coordinates_ + vector.coordinates());
 }
 
-Position Position::operator+(const Position& vector) const
-{
-    return Position(this->coordinates_ + vector.coordinates_);
-}
-
 Position Position::operator*(const float scalar) const
 {
     return Position(this->coordinates_ * scalar);
 }
 
-Position& Position::operator=(const Position& other)
+Position Position::operator/(const float scalar) const
 {
-    this->coordinates_ = other.coordinates_;
-    return *this;
+    return Position(this->coordinates_ / scalar);
 }
 
 bool Position::operator==(const Position& other) const
@@ -60,5 +56,10 @@ bool Position::operator==(const Position& other) const
 }
 
 float Position::distance(const Position& a, const Position& b) { return (b - a).magnitude(); }
+
+Position Position::interpolate(const Position& a, const Position& b, const float t)
+{
+    return a + (b - a) * t;
+}
 
 }  // namespace tap::algorithms::transforms
