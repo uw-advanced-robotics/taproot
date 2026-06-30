f96c8fbd9d8f739e87cb10435277bdbe22b99335
Clementine Allen <callen42@uw.edu>
2026-06-29 21:12:49 -0400

    clang

--- src/tap/communication/serial/ref_serial_data.hpp
+++ src/tap/communication/serial/ref_serial_data.hpp
@@ -342,10 +342,10 @@ public:
                                           ///< only (500 max) if in RMUC, or any robot in RMUL.
             uint16_t bulletsRemaining42;  ///< Number of bullets remaining in hero if in RMUL or 0
                                           ///< if in RMUC.
-            uint16_t projectileAllowanceFortress;  ///< Reserved 17mm projectile allowance
-                                                   ///< provided by Fortress Buff Point.
-            float bulletSpeed;             ///< Last bullet speed (in m/s).
-            float yaw;                     ///< Barrel yaw position (degree).
+            uint16_t projectileAllowanceFortress;         ///< Reserved 17mm projectile allowance
+                                                          ///< provided by Fortress Buff Point.
+            float bulletSpeed;                            ///< Last bullet speed (in m/s).
+            float yaw;                                    ///< Barrel yaw position (degree).
             uint32_t lastReceivedLaunchingInfoTimestamp;  ///< Last time in milliseconds that the
                                                           ///< real-time launching information
                                                           ///< message was received
